623c60f5536481e4478fd8bf49c8a6a1067ce6e9
NamDang311 <namdang.h311@gmail.com>
2017-10-30 11:12:22 +1100

    push to review

--- js/common.js
+++ js/common.js
@@ -1,13 +1,18 @@
 /** Apply Bigtext to titles */
 
 /** Database -> Set base for set 1/2 */
-var countries = ["Australia","Brazil","China","Germany","Great Britain","India","Iran","Kenya","New Zealand","Pakistan","Poland","Russia","South Africa","Ukraine","Vietnam"];
+var countries = ["Australia", "Brazil", "China", "Germany", "Great Britain", "India", "Iran", "Kenya", "New Zealand", "Pakistan", "Poland", "Russia", "South Africa", "Ukraine", "Vietnam"];
 var countriesShuffle = _.shuffle(countries);
-var countriesA= countriesShuffle.splice(0,5);
-var countriesB= countriesShuffle.splice(6,12);
+var countriesA = countriesShuffle.splice(0, 5);
+var countriesB = countriesShuffle.splice(6, 12);
 
-//Opening Splash
+/** Opening Splash Screen */
+
 $(document).ready(function () {
+    TweenMax.to(".blockScreen", 0.3, {
+        autoAlpha: 0,
+        delay: "1"
+    });
     TweenMax.fromTo("#introTypo", 1, {
         scale: 0,
         y: "+=300px"
@@ -21,9 +26,28 @@ $(document).ready(function () {
 
 
 $("#startButton").on("click touchstart", function (event) {
-    TweenMax.staggerTo(".splashScreen, .splashScreen *", 1, {
-        y: "-=100%"
+    TweenMax.to(".splashScreen", 0.2, {
+        autoAlpha: 0,
+        delay: 1
     }, 1);
+    TweenMax.to(".guide", 1, {
+        autoAlpha: 0,
+        delay: 5,
+        onComplete: setStage
+    }, 1);
+});
+
+function setStage() {
+    playSetSound();
+}
+
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('loadingContainer'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data/loading.json' // the path to the animation json
 });
 
 //Set grid
@@ -38,6 +62,7 @@ var $grid = $('.grid').isotope({
     transitionDuration: '0.2s',
     sortBy: 'random'
 });
+
 $grid.isotope('updateSortData').isotope();
 
 
@@ -47,17 +72,16 @@ $grid.isotope('updateSortData').isotope();
 //Load title
 $(".country-title").html(countriesA[0]);
 
-
-
 //1st - Load correct image
-var collectionimg = ["russia","australia","india","south%20africa","china"];
+var collectionimg = ["russia", "australia", "india", "south%20africa", "china"];
 var currentimg = countriesA[0].replace(" ", "%20");
 var correctsrc = "url(../images/flags/" + currentimg + ".png)";
 $(".correct").css("background-image", correctsrc);
+
 //Load random images
-function loadRandomImg (){
-for (var i = 0; i < $(".selection-blocks").length ; i++) {
-    $(".selection-blocks:eq("+ i +"):not(.correct)").css("background-image", "url(../images/flags/" + collectionimg[i] + ".png)");
-}
+function loadRandomImg() {
+    for (var i = 0; i < $(".selection-blocks").length; i++) {
+        $(".selection-blocks:eq(" + i + "):not(.correct)").css("background-image", "url(../images/flags/" + collectionimg[i] + ".png)");
+    }
 }
 loadRandomImg();
--- js/soundfx.js
+++ js/soundfx.js
@@ -1,14 +1,20 @@
 /** Nomal click */
 var normalTune = new Audio('../sound/btnclick.wav');
-
+$("button").on("click touchstart", function (event) {
+    normalTune.play();
+});
 
 /** Play sound when making selection */
 var correctTune = new Audio('../sound/correct.mp3');
 var incorrectTune = new Audio('../sound/correct.wav');
-
+var stagingSound = new Audio('../sound/stage.mp3');
 $(".correct").on("click touchstart", function (event) {
     correctTune.play();
 });
 $(".incorrect").on("click touchstart", function (event) {
     incorrectTune.play();
 });
+
+function playSetSound(){
+    stagingSound.play();
+}
