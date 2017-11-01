d3fd9bcaa92881542c4c5df7ccf592bcb671ea65
NamDang311 <namdang.h311@gmail.com>
2017-11-02 00:39:50 +1100

    Add stage sound and fix logic

--- js/common.js
+++ js/common.js
@@ -6,13 +6,26 @@ var countriesShuffle = _.shuffle(countries);
 var countriesA = countriesShuffle.splice(0, 5);
 var countriesB = countriesShuffle.splice(6, 12);
 
-/** Opening Splash Screen */
+/** Check if video is loaded and remove block screen */
+window.addEventListener('load', function () {
+    var video = document.querySelector('#bgvid');
+    var preloader = document.querySelector('.blockScreen');
 
+    function checkLoad() {
+        if (video.readyState === 4) {
+            TweenMax.to(".blockScreen", 0.3, {
+                autoAlpha: 0,
+                delay: "1"
+            });
+        } else {
+            setTimeout(checkLoad, 100);
+        }
+    }
+    checkLoad();
+}, false);
+
+/** Opening Splash Screen */
 $(document).ready(function () {
-    TweenMax.to(".blockScreen", 0.3, {
-        autoAlpha: 0,
-        delay: "1"
-    });
     TweenMax.fromTo("#introTypo", 1, {
         scale: 0,
         y: "+=300px"
@@ -67,21 +80,50 @@ $grid.isotope('updateSortData').isotope();
 
 
 //****Shuffle randomly
-//$grid.isotope('shuffle');
+//setInterval(function () {
+//    $grid.isotope('shuffle');;
+//}, 1000);
 
 //Load title
 $(".country-title").html(countriesA[0]);
 
 //1st - Load correct image
-var collectionimg = ["russia", "australia", "india", "south%20africa", "china"];
-var currentimg = countriesA[0].replace(" ", "%20");
-var correctsrc = "url(../images/flags/" + currentimg + ".png)";
+var collectionimg = ["russia", "australia", "india", "south%20africa", "china", "kenya", "ukraine"];
+var currentimgName = countriesA[0].replace(" ", "%20");
+var correctsrc = "url(../images/flags/" + currentimgName + ".png)";
 $(".correct").css("background-image", correctsrc);
 
 //Load random images
-function loadRandomImg() {
-    for (var i = 0; i < $(".selection-blocks").length; i++) {
-        $(".selection-blocks:eq(" + i + "):not(.correct)").css("background-image", "url(../images/flags/" + collectionimg[i] + ".png)");
+! function loadRandomImg() {
+    for (var i = 2; i < 5; i++) {
+        $(".selection-blocks-" + i).css("background-image", "url(../images/flags/" + collectionimg[i] + ".png)");
+    }
+}();
+
+/** Score system */
+var blueScore = 0;
+var redScore = 0;
+
+function updateScore() {
+    $(".red-score").html(redScore);
+    $(".blue-score").html(blueScore);
+};
+
+//Load random images -- BLUE : sideNumber = 0, RED : sideNumber = 1  
+function checkSide(value, sideNumber) {
+    if (value == "true") { //If click correct option
+        if (sideNumber == 0) {
+            blueScore++;
+            updateScore();
+        } else {
+            redScore++;
+            updateScore();
+        }
+    } else { //If click incorrect option
+        if (sideNumber < 3) {//blue side
+            $(".block-area").addClass("top-half");
+        } else {//red side
+            
+        }
     }
 }
-loadRandomImg();
--- js/soundfx.js
+++ js/soundfx.js
@@ -1,20 +1,39 @@
 /** Nomal click */
-var normalTune = new Audio('../sound/btnclick.wav');
+
+var normalTune = new Howl({
+    src: ['../sound/btnclick.wav'],
+});
+
 $("button").on("click touchstart", function (event) {
     normalTune.play();
 });
 
 /** Play sound when making selection */
 var correctTune = new Audio('../sound/correct.mp3');
-var incorrectTune = new Audio('../sound/correct.wav');
-var stagingSound = new Audio('../sound/stage.mp3');
+var incorrectTune = new Audio('../sound/incorrect.mp3');
+
 $(".correct").on("click touchstart", function (event) {
+    checkSide("true", $(".correct").index(this));
     correctTune.play();
 });
 $(".incorrect").on("click touchstart", function (event) {
+    checkSide("false", $(".incorrect").index(this));
+    console.log($(".incorrect").index(this));
     incorrectTune.play();
 });
 
-function playSetSound(){
+/** Staging fx*/
+var stagingSound = new Howl({
+    src: ['../sound/stage.mp3'],
+    onend: function () {
+        sceneSound_1.play();
+    }
+});
+var sceneSound_1 = new Howl({
+    src: ['../sound/soundtracks/1.mp3'],
+});
+stagingSound.play();
+
+function playSetSound() {
     stagingSound.play();
 }
